af6e21c4e2675f2062b4968a478a2a7e8b2d6984
Jiilan Nashrulloh Tanjung <jiilan3maret2008@gmail.com>
2023-06-30 05:14:59 +0700

    Initial commit

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,83 @@
+<x-guest-layout>
+    <x-auth-card>
+        <x-slot name="logo">
+            <a href="/">
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            </a>
+        </x-slot>
+
+        <!-- Validation Errors -->
+        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
+
+            <!-- Name -->
+            <div>
+                <x-label for="name" :value="__('Name')" />
+
+                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+            </div>
+
+            <!-- Email Address -->
+            <div class="mt-4">
+                <x-label for="email" :value="__('Email')" />
+
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+            </div>
+
+            <div class="mt-4">
+                <x-label for="nomorHP" :value="__('nomorHP')" />
+
+                <x-input id="nomorHP" class="block mt-1 w-full" type="number" name="nomorHP" :value="old('nomorHP')" required />
+            </div>
+
+            <div class="mt-4">
+                <x-label for="noRek" :value="__('noRek')" />
+
+                <x-input id="noRek" class="block mt-1 w-full" type="number" name="noRek" :value="old('noRek')" required />
+            </div>
+
+            <!-- Nama Bank -->
+             <div class="mt-4">
+                <x-input-label for="bank" :value="__('Nama Bank')" />
+                <select id="bank" name="bank" class="block w-full mt-1 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300">
+                    <option value="BCA">BCA</option>
+                    <option value="BRI">BRI</option>
+                    <option value="BNI">BNI</option>
+                    <!-- Tambahkan opsi bank lainnya sesuai kebutuhan -->
+                </select>
+                <x-input-error :messages="$errors->get('bank')" class="mt-2" />
+             </div>
+
+            <!-- Password -->
+            <div class="mt-4">
+                <x-label for="password" :value="__('Password')" />
+
+                <x-input id="password" class="block mt-1 w-full"
+                                type="password"
+                                name="password"
+                                required autocomplete="new-password" />
+            </div>
+
+            <!-- Confirm Password -->
+            <div class="mt-4">
+                <x-label for="password_confirmation" :value="__('Confirm Password')" />
+
+                <x-input id="password_confirmation" class="block mt-1 w-full"
+                                type="password"
+                                name="password_confirmation" required />
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+                    {{ __('Already registered?') }}
+                </a>
+
+                <x-button class="ml-4">
+                    {{ __('Register') }}
+                </x-button>
+            </div>
+        </form>
+    </x-auth-card>
+</x-guest-layout>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,16 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-pink-200">
+    <div class="p-10 pl-80 pr-60 flex gap-x-10 justify-between items-center">
+        <a href="/"><img src="img/logo.jpg" alt="Logo"></a>
+        <ul class="flex gap-x-10">
+            <li><a href="{{ route('login') }}" class="px-4 py-2 bg-blue-500 text-white rounded-lg">Masuk</a></li>
+            <li><a href="{{ route('register') }}" class="px-4 py-2 bg-blue-500 text-white rounded-lg">Daftar</a></li>
+        </ul>
+    </div>
+</body>
