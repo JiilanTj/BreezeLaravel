d75684c7b569fc44ae906a7d2543a24c983206e3
Macbook Pro <macbookpro@MACBOOKs-MacBook-Pro.local>
2023-07-01 18:10:16 +0700

    buat migrate transaksi

---
+++ database/migrations/2023_07_01_110826_create_transaksi_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransaksiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaksi', function (Blueprint $table) {
+            $table->timestamps();
+            $table->bigIncrements('id');
+            $table->string('bank');
+            $table->string('noRek');
+            $table->string('transaksi');
+            $table->decimal('nominal', 10, 2);                        
+            $table->timestamp('timestamp_perubahan_status')->nullable();
+            $table->string('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksi');
+    }
+}
