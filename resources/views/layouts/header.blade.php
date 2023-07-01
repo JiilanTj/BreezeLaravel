c5df66aef886145d0710938e36254195def8a463
Macbook Pro <macbookpro@MACBOOKs-MacBook-Pro.local>
2023-07-01 19:59:30 +0700

    buat migrate transaksi

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,16 +6,16 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <div class="flex lg:flex-row md:flex-col sm:flex-col bg-black px-6 py-4 justify-between items-center">
+    <div class="flex lg:flex-row md:flex-row sm:flex-col bg-black px-6 py-1 justify-between items-center">
     <div class="h-16 py-2">
         <img src="{{ asset('assets/logo.png') }}" alt="Logo" class="h-full">
     </div>
         <div class="items-center md:flex-row gap-x-8 w-3/8">
             <button class="bg-purple-800 mx-2 px-4 py-1 text-white rounded-lg hover:bg-blue-600">
-                <a href="{{ route('login') }}">Masuk</a>
+                <a href="/login">Masuk</a>
             </button>
             <button class="bg-purple-800 mx-2 px-4 py-1 text-white rounded-lg hover:bg-blue-600">
-                <a href="{{ route('register') }}">Daftar</a>
+                <a href="/register">Daftar</a>
             </button>
         </div>
     </div>
