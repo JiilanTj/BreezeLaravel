7aabf67f63807b1b026d27d000091bb5563b7d8d
Macbook Pro <macbookpro@MACBOOKs-MacBook-Pro.local>
2023-07-01 18:39:09 +0700

    buat migrate transaksi

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,12 +5,19 @@
     <title>Website</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="bg-pink-200">
-    <div class="p-10 pl-80 pr-60 flex gap-x-10 justify-between items-center">
-    <img src="{{ asset('assets/logo.png') }}" alt="Logo" width="50" height="50">
-        <ul class="flex gap-x-10">
-            <li><a href="{{ route('login') }}" class="px-4 py-2 bg-blue-500 text-white rounded-lg">Masuk</a></li>
-            <li><a href="{{ route('register') }}" class="px-4 py-2 bg-blue-500 text-white rounded-lg">Daftar</a></li>
-        </ul>
+<body>
+    <div class="flex lg:flex-row md:flex-col sm:flex-col bg-black px-6 py-4 justify-between items-center">
+    <div class="h-16 py-2">
+        <img src="{{ asset('assets/logo.png') }}" alt="Logo" class="h-full">
     </div>
+        <div class="items-center md:flex-row gap-x-8 w-3/8">
+            <button class="bg-purple-800 mx-2 px-4 py-1 text-white rounded-lg hover:bg-blue-600">
+                <a href="{{ route('login') }}">Masuk</a>
+            </button>
+            <button class="bg-purple-800 mx-2 px-4 py-1 text-white rounded-lg hover:bg-blue-600">
+                <a href="{{ route('register') }}">Daftar</a>
+            </button>
+        </div>
+    </div>
+    
 </body>
