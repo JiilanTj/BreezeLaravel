384c8afb5f9d131bca48936f1f314764bced3e08
Jiilan Nashrulloh Tanjung <jiilan3maret2008@gmail.com>
2023-07-04 08:58:54 +0700

    Angka 4 biji baru bisa

--- resources/views/macau4d.blade.php
+++ resources/views/macau4d.blade.php
@@ -15,6 +15,18 @@
         }
     </style>
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script>
+        function validateInputLength(inputId, jumlahId) {
+            var input = document.getElementById(inputId);
+            var jumlah = document.getElementById(jumlahId);
+
+            if (input.value.length !== 4) {
+                jumlah.disabled = true;
+            } else {
+                jumlah.disabled = false;
+            }
+        }
+    </script>
 </head>
 <body>
 @include('layouts.navigation')
@@ -35,14 +47,13 @@
             @csrf
 
             @for ($i = 1; $i <= 10; $i++)
-    <div class="flex mb-4">
-        <label for="angka{{ $i }}" class="w-16">Angka {{ $i }}:</label>
-        <input type="number" id="angka{{ $i }}" name="angka{{ $i }}" class="border border-gray-300 px-2 py-1 rounded">
-        <label for="jumlah{{ $i }}" class="w-16">Jumlah {{ $i }}:</label>
-        <input type="number" id="jumlah{{ $i }}" name="jumlah{{ $i }}" class="border border-gray-300 px-2 py-1 rounded">
-    </div>
+                <div class="flex mb-4">
+                    <label for="angka{{ $i }}" class="w-16">Angka {{ $i }}:</label>
+                    <input type="number" id="angka{{ $i }}" name="angka{{ $i }}" class="border border-gray-300 px-2 py-1 rounded" oninput="validateInputLength('angka{{ $i }}', 'jumlah{{ $i }}')">
+                    <label for="jumlah{{ $i }}" class="w-16">Jumlah {{ $i }}:</label>
+                    <input type="number" id="jumlah{{ $i }}" name="jumlah{{ $i }}" class="border border-gray-300 px-2 py-1 rounded" disabled>
+                </div>
             @endfor
-
 
             <div class="flex justify-center">
                 <button type="submit" class="bg-green-500 text-white px-4 py-2 rounded">Submit</button>
